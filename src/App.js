3811715610e2a12b791a9fef7ca4dbd1bdf871b1
SEIMIN123 <interkp12@gmail.com>
2022-09-14 14:02:26 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header(props) {
+    console.log('props', props, props.title);
+    return <header>
+        <h1>
+            <a href='/'>{props.title}</a>
+        </h1>
+    </header>
 }
 
+function Nav(props) {
+    const list = [
+
+    <li><a href='/read/1'>html</a></li>,
+    <li><a href='/read/2'>css</a></li>,
+    <li><a href='/read/3'>js</a></li>
+    
+  ]
+
+  for(let i = 0; i < props.name.length; i++){
+
+    let a = props.name[i];
+    list.push(<li key = {a.id}><a href={'/read/'+a.id}>{a.title}</a></li>)
+  }
+
+    return <nav>
+        <ol>
+          {list}
+        </ol>
+    </nav>
+}
+
+function Article(props) {
+
+    return <article>
+        <h2>{props.title}</h2>
+        {props.body}
+    </article>
+}
+
+// ----------------------------------------------
+
+function App() {
+
+    const name = [
+
+        {
+            id: 1,
+            title: 'html',
+            body: 'html is ...'
+        }, {
+            id: 2,
+            title: 'css',
+            body: 'css is ...'
+        }, {
+            id: 3,
+            title: 'javascript',
+            body: 'javascript is ...'
+        }
+
+    ]
+    return (
+        <div>
+            <Header title="REACT"></Header>
+            <Nav name={name}></Nav>
+            <Article title="Welcome" body="Hello, Web"></Article>
+            <Article title="Bye" body="bye, Web"></Article>
+        </div>
+    );
+}
 export default App;
