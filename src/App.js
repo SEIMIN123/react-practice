97cff96cf48c7e8e1c9a56286ecaa0f50877f55d
SEIMIN123 <interkp12@gmail.com>
2022-09-14 14:18:25 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 
-function Header(props) {
-    console.log('props', props, props.title);
-    return <header>
-        <h1>
-            <a href='/'>{props.title}</a>
-        </h1>
-    </header>
-}
 
-function Nav(props) {
-    const list = [
-
-    <li><a href='/read/1'>html</a></li>,
-    <li><a href='/read/2'>css</a></li>,
-    <li><a href='/read/3'>js</a></li>
-    
-  ]
-
-  for(let i = 0; i < props.name.length; i++){
-
-    let a = props.name[i];
-    list.push(<li key = {a.id}><a href={'/read/'+a.id}>{a.title}</a></li>)
-  }
-
-    return <nav>
-        <ol>
-          {list}
-        </ol>
-    </nav>
-}
 
 function Article(props) {
 
@@ -38,6 +8,53 @@ function Article(props) {
         <h2>{props.title}</h2>
         {props.body}
     </article>
+}
+
+function Header(props) {
+    console.log('props', props, props.title);
+    return <header>
+        <h1>
+            <a
+                href='/'
+                onClick={(event) => {
+                    event.preventDefault();
+                    props.onChangeMode();
+                }}>{props.title}</a>
+        </h1>
+    </header>
+}
+
+function Nav(props) {
+  const list = [
+
+      <li>
+          <a href='/read/1'>html</a>
+      </li>,
+      <li>
+          <a href='/read/2'>css</a>
+      </li>,
+      <li>
+          <a href='/read/3'>js</a>
+      </li>
+  ]
+
+  for (let i = 0; i < props.name.length; i++) {
+
+      let b = props.name[i];
+      list.push(<li key={b.id}>
+
+          <a id={b.id} href={'/read/' + b.id} onClick={event=>{
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}>{b.title}</a>
+      </li>)
+  }
+
+  return <nav>
+      <ol>
+          {list}
+      </ol>
+  </nav>
 }
 
 // ----------------------------------------------
@@ -63,8 +80,14 @@ function App() {
     ]
     return (
         <div>
-            <Header title="REACT"></Header>
-            <Nav name={name}></Nav>
+            <Header
+                title="REACT"
+                onChangeMode={() => {
+                    alert('Header');
+                }}></Header>
+            <Nav name={name} onChangeMode = {(id)=>{
+              alert(id);
+            }}></Nav>
             <Article title="Welcome" body="Hello, Web"></Article>
             <Article title="Bye" body="bye, Web"></Article>
         </div>
